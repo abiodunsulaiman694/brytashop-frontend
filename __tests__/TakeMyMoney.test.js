3071121fe208f1c51735997dc2506099cd260e64
Abiodun Sulaiman (FullStack Developer) <hello@abiodun.me>
2019-09-29 14:18:20 +0100

    Ready to deploy

--- __tests__/TakeMyMoney.test.js
+++ __tests__/TakeMyMoney.test.js
@@ -13,9 +13,7 @@ Router.router = { push() {} };
 
 const mocks = [
   {
-    request: {
-      query: CURRENT_USER_QUERY
-    },
+    request: { query: CURRENT_USER_QUERY },
     result: {
       data: {
         me: {
@@ -27,7 +25,7 @@ const mocks = [
   }
 ];
 
-describe("<TakeMyMoney />", () => {
+describe("<TakeMyMoney/>", () => {
   it("renders and matches snapshot", async () => {
     const wrapper = mount(
       <MockedProvider mocks={mocks}>
@@ -37,6 +35,66 @@ describe("<TakeMyMoney />", () => {
     await wait();
     wrapper.update();
     const checkoutButton = wrapper.find("ReactStripeCheckout");
-    expect(toJSON(checkoutButton)).toMatchSnapshot;
+    expect(toJSON(checkoutButton)).toMatchSnapshot();
+  });
+  it("creates an order ontoken", async () => {
+    const createOrderMock = jest.fn().mockResolvedValue({
+      data: { createOrder: { id: "xyz789" } }
+    });
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <TakeMyMoney />
+      </MockedProvider>
+    );
+    const component = wrapper.find("TakeMyMoney").instance();
+    // manully call that onTokenResponse method
+    component.onTokenResponse({ id: "abc123" }, createOrderMock);
+    expect(createOrderMock).toHaveBeenCalled();
+    expect(createOrderMock).toHaveBeenCalledWith({
+      variables: { token: "abc123" }
+    });
+  });
+
+  it("turns the progress bar on", async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <TakeMyMoney />
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    NProgress.start = jest.fn();
+    const createOrderMock = jest.fn().mockResolvedValue({
+      data: { createOrder: { id: "xyz789" } }
+    });
+    const component = wrapper.find("TakeMyMoney").instance();
+    // manully call that onTokenResponse method
+    component.onTokenResponse({ id: "abc123" }, createOrderMock);
+    expect(NProgress.start).toHaveBeenCalled();
+  });
+
+  it("routes to the order page when completed", async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <TakeMyMoney />
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    const createOrderMock = jest.fn().mockResolvedValue({
+      data: { createOrder: { id: "xyz789" } }
+    });
+    const component = wrapper.find("TakeMyMoney").instance();
+    Router.router.push = jest.fn();
+    // manully call that onTokenResponse method
+    component.onTokenResponse({ id: "abc123" }, createOrderMock);
+    await wait();
+    expect(Router.router.push).toHaveBeenCalled();
+    expect(Router.router.push).toHaveBeenCalledWith({
+      pathname: "/order",
+      query: {
+        id: "xyz789"
+      }
+    });
   });
 });
